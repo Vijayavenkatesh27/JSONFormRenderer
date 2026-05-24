d451131c79b8a6109f3dc947749bf2f46f67a28c
Vijaya-venkatesh <vijayagovindaraj@bitcot.com>
2026-05-24 19:26:12 +0530

    Initial Commit

---
+++ JSONFormRenderer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JSONFormRenderer
+//
+//  Created by bitcot on 24/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JSONFormRenderer/JSONFormRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONFormRendererApp.swift
+//  JSONFormRenderer
+//
+//  Created by bitcot on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct JSONFormRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
